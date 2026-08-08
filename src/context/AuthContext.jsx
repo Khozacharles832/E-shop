20c68e2e9a3d5ac4f7c5f8e82843044cc8a597ac
Charlie <khozacharles832@gmail.com>
2026-08-08 13:49:21 +0200

    Added auth

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,52 @@
+import { createContext, useState } from "react";
+import { set } from "react-hook-form";
+
+export const AuthContext = createContext();
+
+export default function AuthProvider({ children }) {
+  const [user, setUser] = useState(
+    localStorage.getItem("currentUserEmail")
+      ? { email: localStorage.getItem("currentUserEmail") }
+      : null,
+  );
+
+  function signUp(email, password) {
+    const users = JSON.parse(localStorage.getItem("users") || "[]");
+
+    if (users.find((u) => u.email === email)) {
+      return { success: false, message: "User already exists" };
+    }
+    const newUser = { email, password };
+    users.push(newUser);
+    localStorage.setItem("users", JSON.stringify(users));
+    localStorage.setItem("currentUserEmail", email);
+    setUser({ email });
+
+    return { success: true, message: "User created successfully" };
+  }
+
+  function login(email, password) {
+    const users = JSON.parse(localStorage.getItem("users") || "[]");
+    const user = users.find(
+      (u) => u.email === email && u.password === password,
+    );
+
+    if (!user) {
+      return { success: false, error: "Invalid email or password" };
+    }
+    localStorage.setItem("currentUserEmail", email);
+    setUser({ email });
+    return { success: true, message: "Login successful" };
+  }
+
+  function logout() {
+    localStorage.removeItem("currentUserEmail");
+    setUser(null);
+  }
+
+  return (
+    <AuthContext.Provider value={{ signUp, login, logout, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
